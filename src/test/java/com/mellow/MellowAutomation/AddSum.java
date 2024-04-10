18c7ac2ec847ff89ead791b5a7cf43d214a2670e
sreelajamukundakumaran <sreelajamukundakumaran@sreelajas-mbp>
2024-04-10 21:33:28 +0100

    1st commit to branch

---
+++ src/test/java/com/mellow/MellowAutomation/AddSum.java
@@ -0,0 +1,28 @@
+package com.mellow.MellowAutomation;
+
+public class AddSum {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int a = 190;
+		int b = 20;
+		
+		Sum(a, b);						
+
+	}
+	
+	public static void Sum(int a,int b) {
+		
+		
+		int c = a + b;
+		System.out.println(c);
+		
+	}
+
+}
+
+// whats the use of parameterising?
+//Why cant I declare a and b outside the class eventhough its public?
+//why static
