8c37e89e686319c77aa1084758e2d4368a24f89a
sreelajamukundakumaran <sreelajamukundakumaran@sreelajas-mbp>
2024-04-11 14:32:15 +0100

    hungrycommit4

---
+++ src/test/java/com/mellow/MellowAutomation/Hungry.java
@@ -0,0 +1,25 @@
+package com.mellow.MellowAutomation;
+
+public class Hungry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry=="yes") {
+		
+			System.out.println("go eat");
+		}
+		
+			else 
+				
+				System.out.println("go work");
+			
+			
+			
+		}
+
+	}
+
+
